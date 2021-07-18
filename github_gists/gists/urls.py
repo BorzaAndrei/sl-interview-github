de291f5974849ce0717e1f408d8d952d27ee7775
BorzaAndrei <andreiborza98@gmail.com>
2021-07-18 20:25:39 +0300

    Completed search, language badges, forks owner badges

--- github_gists/gists/urls.py
+++ github_gists/gists/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search_username/', views.search_username, name='search_username')
 ]
--- github_gists/gists/views.py
+++ github_gists/gists/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+import requests
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the gists index.")
+    return render(request, "gists/base.html")
+
+
+def search_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        r = requests.get(f"https://api.github.com/users/{username}/gists", headers={"Accept": "application/vnd.github.v3+json"})
+        # TODO: paging
+        content = r.json()
+        if r.status_code == 200:
+            for gist in content:
+                # Get languages used in files
+                languages = []
+                for file in gist["files"]:
+                    languages.append(gist["files"][file]['language'])
+                gist["languages"] = list(set(languages))
+
+                # Get owners of forks
+                r_forks = requests.get(gist["forks_url"], headers={"Accept": "application/vnd.github.v3+json"})
+                # TODO: Sort for last used
+                forks = []
+                for fork in r_forks.json():
+                    forks.append(fork["owner"]["login"])
+                gist["forks"] = forks[:3]
+            return render(request, "gists/base.html", {"username": username, "json_content": content})
+    return render(request, "gists/base.html")
